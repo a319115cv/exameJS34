b4197689e081c1c432e620685b40ff0252ea0be0
a319115cv <44172458+a319115cv@users.noreply.github.com>
2019-02-06 13:51:02 +0100

    Add files via upload

---
+++ exameJS34/exame34.js
@@ -0,0 +1,42 @@
+document.write('Espero que te guste o xogo')
+x=prompt("Traduce do inglés mouse ao galego")
+while(x !="rato") {
+	alert("Equivocacheste ao tradicir, sintoo, teras que facelo outra vez")
+	x= prompt("Traduce do inglés mouse ao galego") ;
+
+}
+
+alert("Ben feito, acertaches")
+	
+y= prompt("Agora sera do reves, traduce do galego teclado ao inglés")
+while(y !="keyboard") {
+	alert("Equivocacheste ao tradicir, sintoo, teras que facelo outra vez")
+	y= prompt("Veña outra vez, traduce do galego teclado ao inglés")
+}
+
+alert("Ben!!, acertaches de novo")
+
+z= prompt("Agora sera do galego arbol ao ingles")
+while(z !="tree") {
+	alert("Cachis, fallaste, volveo a intentar")
+	z= prompt("Agora sera do galego arbol ao ingles")
+}
+
+alert("Urra acertaches")
+
+
+b= prompt("Traduce do castelan coche ao ingles")
+while(b !="car") {
+	alert("A laa, fallaste, intentao outra vez")
+	b= prompt("Traduce do castelan coche ao ingles")
+}
+
+alert("Acertaches, era facil eh ;)")
+
+a= prompt("Traduce do ingles mirror ao galego")
+while(a !="espello") {
+	alert("Fallaste, intentao outra vez")
+	a= prompt("Traduce do ingles mirror ao galego")
+}
+
+alert("Gañaste")
